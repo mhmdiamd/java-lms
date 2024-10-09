2555b2ce4077bf82cdf3574923abeb7e30f8bf0b
mhmdiamd <m1pro2021@mhmdiamd-2.local>
2024-10-09 20:01:56 +0700

    init

---
+++ domain/model/entity/User.java
@@ -0,0 +1,7 @@
+package com.java_lms.domain.model.entity;
+
+public class User {
+  public static void printName(String name) {
+    System.out.println(name);
+  }
+}
---
+++ main/Main.java
@@ -0,0 +1,11 @@
+package com.java_lms.main;
+
+import com.ddd_ecommerce.domain.model.entity.User;
+
+public class Main {
+  public static void main(String[] args) {
+    User userModel = new User();
+
+    userModel.printName("ilham");
+  };
+}
